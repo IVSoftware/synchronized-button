f9e8bbb7b90d16b827e23801b19005881f2a13ee
Tom Gregor <tgregor@ivsoftware.com>
2024-03-15 07:15:03 -0600

    Add project files.

---
+++ synchronized-button/MainForm.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace static_button
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            buttonSetting = new Button();
+            buttonShowForm1 = new Button();
+            buttonShowForm2 = new Button();
+            SuspendLayout();
+            // 
+            // buttonSetting
+            // 
+            buttonSetting.Location = new Point(43, 42);
+            buttonSetting.Name = "buttonSetting";
+            buttonSetting.Size = new Size(171, 34);
+            buttonSetting.TabIndex = 0;
+            buttonSetting.Text = "Setting";
+            buttonSetting.UseVisualStyleBackColor = true;
+            // 
+            // buttonShowForm1
+            // 
+            buttonShowForm1.Location = new Point(43, 82);
+            buttonShowForm1.Name = "buttonShowForm1";
+            buttonShowForm1.Size = new Size(171, 34);
+            buttonShowForm1.TabIndex = 0;
+            buttonShowForm1.Text = "Show Form 1";
+            buttonShowForm1.UseVisualStyleBackColor = true;
+            // 
+            // buttonShowForm2
+            // 
+            buttonShowForm2.Location = new Point(43, 122);
+            buttonShowForm2.Name = "buttonShowForm2";
+            buttonShowForm2.Size = new Size(171, 34);
+            buttonShowForm2.TabIndex = 0;
+            buttonShowForm2.Text = "Show Form 2";
+            buttonShowForm2.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(478, 244);
+            Controls.Add(buttonShowForm2);
+            Controls.Add(buttonShowForm1);
+            Controls.Add(buttonSetting);
+            Name = "MainForm";
+            Text = "Main Form";
+            StartPosition = FormStartPosition.CenterScreen;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button buttonSetting;
+        private Button buttonShowForm1;
+        private Button buttonShowForm2;
+    }
+}
---
+++ synchronized-button/MainForm.cs
@@ -0,0 +1,94 @@
+namespace static_button
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            buttonSetting.BackColorChanged += (sender, e) =>
+            {
+                if (
+                    sender is Button srce
+                    &&
+                    string.Equals(srce.Name, "buttonSetting"))
+                {
+                    foreach (var form in Application.OpenForms.OfType<MainForm>())
+                    {
+                        // Find the button on each form and update its color.
+                        if (form.Controls["buttonSetting"] is Button dest)
+                        {
+                            dest.BackColor = srce.BackColor;
+                        }
+                    }
+                }
+            };
+
+            // Toggle color
+            buttonSetting.Click += (sender, e) =>
+                buttonSetting.BackColor = buttonSetting.BackColor.Equals(Color.LightSkyBlue) ?
+                Color.FromArgb(151, 242, 0) : Color.LightSkyBlue;
+
+            buttonShowForm1.Click += (sender, e) =>
+            {
+                var mainForm = Application.OpenForms["MainForm"];
+                if (
+                    Application.OpenForms["f1"] is Form form
+                    &&
+                    !form.Visible)
+                {
+                    form.Show(mainForm);
+                }
+                else
+                {
+                    new InheritedForm
+                    {
+                        Text = "Inherited Form 1",
+                        Name = "f1",
+                        StartPosition = FormStartPosition.Manual,
+                        Size = mainForm.Size,
+                        Location = new Point(mainForm.Left + mainForm.Width + 10, mainForm.Top),
+                    }.Show(mainForm);
+                }
+            };
+            buttonShowForm2.Click += (sender, e) =>
+            {
+                var mainForm = Application.OpenForms["MainForm"];
+                if (
+                    Application.OpenForms["f2"] is Form form
+                    &&
+                    !form.Visible)
+                {
+                    form.Show(mainForm);
+                }
+                else
+                {
+                    new InheritedForm
+                    { 
+                        Text = "Inherited Form 2",
+                        Name = "f2",
+                        StartPosition = FormStartPosition.Manual,
+                        Size = mainForm.Size,
+                        Location = new Point(
+                            mainForm.Left + mainForm.Width + 10, 
+                            mainForm.Top + mainForm.Height + 10),
+                    }.Show(mainForm);
+                }
+            };
+        }
+    }
+    public class InheritedForm : MainForm 
+    {
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            if(e.CloseReason.Equals(CloseReason.UserClosing))
+            {
+                e.Cancel = true;
+                Hide();
+                Application.OpenForms["MainForm"]?.BringToFront(); // Workaroud 'last visible child' issue
+            }
+            base.OnFormClosing(e);
+        }
+    }
+}
+
+
